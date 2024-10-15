2407201231dfb46e78c2a511b893e8807ad9a5ee
Bhoami K Khona <bhoamikhona@gmail.com>
2024-10-15 17:40:24 +0530

    added h1 and fixed footer css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function Header() {
   return (
     <header className="header">
+      <h1 className="visually-hidden">Tip Calculator</h1>
       <div className="logo-container">
         <img className="logo" src="./images/logo.svg" alt="splitter logo" />
       </div>
